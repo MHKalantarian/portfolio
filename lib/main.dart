ac236776167f2fdc25f05b797a7d55fc3712d0d0
MHK <MHKalantarian@gmail.com>
2021-09-26 20:58:24 +0330

    Updated assets

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MHKSoft',
+      title: 'MHK Soft',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
