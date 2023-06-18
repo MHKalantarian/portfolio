6c0590b4a1e562599f08bd1930b8c0fa271d7a8a
MHKalantarian <MHKalantarian@gmail.com>
2023-06-18 08:50:40 +0330

    Minor improvements

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'MHKalantarian',
+      title: 'MHKalantarian Portfolio',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
